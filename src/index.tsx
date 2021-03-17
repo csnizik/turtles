c72528cace7bcbc1f1cd8ef9f19e24dc91e0fec1
Ashim Ghimire <aghimire85@gmail.com>
2021-03-17 13:09:24 -0700

    Dynamic data shown on landing page using typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CongressionalReport from './app/CongressionalReport';
+import './stylesheets/app.css';
 
-const App = ()=> {
-  return(
-    <div>
-      Congressional Report
-      
+const App = () => {
+  return (
+    <div className='container'>
+      <CongressionalReport />
     </div>
-  )
-}
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
