583f4ec4ee1511859fa3f6f3a3b6bcbe752201f1
Ashim Ghimire <aghimire85@gmail.com>
2021-03-17 08:29:44 -0700

    Created typescript create-react-app

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = ()=> {
+  return(
+    <div>
+      Congressional Report
+      
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
