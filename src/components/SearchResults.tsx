a2ae2e286fd4963cde5e68467a6cdc5f21290c0a
Brandon Beschta <brandonbeschta@Brandons-MacBook-Pro.local>
2021-04-06 11:35:47 -0500

    Map over results

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -5,19 +5,20 @@ const SearchResults = ({queryResults} : any) => {
   const [results, setResult] = useState(queryResults);
 
   useEffect(() => {
-    console.log("Query results: ", queryResults)
     if (queryResults) {
-          setResult(queryResults);
+      setResult(queryResults);
     }
   }, [queryResults])
 
-  if (!results || !results.features) return null;
+  console.log("Results ", results)
+
+  if (!results || !results.features.length) return null;
 
   return (
     <div>
       <h3>Search results</h3>
       <hr />
-      {result && result.features.map((feature: any) => {
+      {results && results.features.map((feature: any) => {
         return (
           <div key={feature.attributes.objectid_1}>
             <p>State Abbreviation: {feature.attributes.state_abbr}</p>
