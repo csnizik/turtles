289d4500107f6022bd0d3caf936cf33ec9e5fc70
Zane Belkhayat <zane.belkhayat@spatialfront.com>
2021-08-19 17:04:16 +0000

    Merged in CIG-356-tooltips (pull request #176)
    CIG-356 tooltips
    
    * Added info popups with sample text to landuse section of quick search
    
    * Add descriptions for land use types
    
    * Merge branch 'CIG-356-tooltips' of https://bitbucket.org/gassomsfi/greyworm-epi into CIG-356-tooltips
    
    
    Approved-by: brandon.beschta
    Approved-by: Bill Luttrell

--- src/components/LandUseSection/LandUseSection.tsx
+++ src/components/LandUseSection/LandUseSection.tsx
@@ -1,19 +1,58 @@
 import './land-use-section.scss';
+import { Tooltip } from 'reactstrap';
+import { useState } from 'react';
 
 interface ILandUseData {
   id: number;
   label: string;
+  description: string;
 }
 
 const exampleLandUseData: ILandUseData[] = [
-  { id: 0, label: 'Crop' },
-  { id: 1, label: 'Forest' },
-  { id: 2, label: 'Farm and Other Rural' },
-  { id: 3, label: 'Range' },
-  { id: 4, label: 'Pasture' },
+  {
+    id: 0,
+    label: 'Cropland',
+    description:
+      'Includes areas used for the production of adapted crops for harvest',
+  },
+  {
+    id: 1,
+    label: 'Rangeland',
+    description:
+      'Lands on which the indigenous vegetation is predominately grasses, grass-like plants, forbs, and possibly shrubs or dispersed trees',
+  },
+  {
+    id: 2,
+    label: 'Developed Land / Urban Ag',
+    description: 'Sample description of Developed Land / Urban Ag',
+  },
+  {
+    id: 3,
+    label: 'Forestland',
+    description:
+      'Forest lands provide clean air, carbon sequestration, climate change buffering, flood protection, wildlife habitat, recreation and aesthetic enjoyment.',
+  },
+  {
+    id: 4,
+    label: 'Pasture',
+    description:
+      'Vegetation cover comprised primarily of introduced or enhanced native forage species that is used for livestock grazing',
+  },
+  {
+    id: 5,
+    label: 'Other Farm and Rural Land',
+    description: 'Sample description of Other Farm and Rural land',
+  },
 ];
 
 const LandUseSection = () => {
+  const [tooltipOpen, setTooltipOpen] = useState([false]);
+  const toggleTooltip = (index) => {
+    const newTipOpen = [...tooltipOpen];
+    newTipOpen[index] = !newTipOpen[index];
+    setTooltipOpen(newTipOpen);
+  };
+
   return (
     <fieldset className='usa-fieldset'>
       <legend className='usa-legend'>Filter By Land Use</legend>
@@ -35,6 +74,20 @@ const LandUseSection = () => {
                 >
                   {landType.label}
                 </label>
+                <i
+                  className='fas fa-info-circle'
+                  aria-hidden='true'
+                  id={`tooltip${landType.id}`}
+                />
+
+                <Tooltip
+                  placement='right'
+                  isOpen={tooltipOpen[landType.id]}
+                  target={`tooltip${landType.id}`}
+                  toggle={() => toggleTooltip(landType.id)}
+                >
+                  {landType.description}
+                </Tooltip>
               </div>
             );
           })}
