ae60f90aa4d5c684a37561c5564c4f78af2f0c07
Ashim Ghimire <aghimire85@gmail.com>
2021-08-23 12:51:48 -0700

    All Location API are moved to RTK Query

--- src/containers/CustomSearchContainer/CustomSearchContainer.tsx
+++ src/containers/CustomSearchContainer/CustomSearchContainer.tsx
@@ -1,13 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { getRequest } from '../../common/util/AxiosUtil';
-import './custom-search.scss';
 import CustomButton from '../../components/CustomButton';
 import SearchByLocation from '../../components/SearchByLocation';
 import SearchByResourceConcern from '../../components/SearchByResourceConcern';
 import LandUseSection from '../../components/LandUseSection';
 import SearchByConservationPractice from '../../components/SearchByConservationPractice';
-import LocationSearch from '../../components/LocationSearch';
+import './custom-search.scss';
 
 interface ISearchInput {
   stateSelect: number;
@@ -21,37 +19,11 @@ const defaultSearchInput: ISearchInput = {
 
 const CustomSearchContainer = () => {
   const [searchInput, setSearchInput]: any = useState(defaultSearchInput);
-  const [statesList, setStatesList]: any = useState([]);
-  const [countyList, setCountyList]: any = useState([]);
+
   const { t } = useTranslation();
 
-  useEffect(() => {
-    async function fetchStateList() {
-      const response = await getRequest('/states');
-      setStatesList(response.data);
-    }
-
-    fetchStateList();
-  }, []);
-
-  async function fetchCountyListPerStateCode(stateCode: any) {
-    const countyResponse = await getRequest(`/counties/${stateCode}`);
-    setCountyList(countyResponse.data);
-  }
-
   const handleSearch = () => {
-    console.log('TODO: Submit form for search', searchInput);
-  };
-
-  const handleInputChange = (e: any) => {
-    const { name, value } = e.target;
-    if (name === 'stateSelect' && value) {
-      fetchCountyListPerStateCode(value);
-      if (searchInput.stateSelect >= 0 && searchInput.stateSelect !== value) {
-        setCountyList([]);
-      }
-    }
-    setSearchInput({ ...searchInput, [name]: value });
+    console.log('TODO: Submit form for search');
   };
 
   return (
