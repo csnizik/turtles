4cbe3aabec072642e28c0e39cf26f263a8b67379
Mac <mac@MacBook-Pro.local>
2021-08-30 14:09:07 -0700

    fix code coverage

--- jest.config.js
+++ jest.config.js
@@ -9,5 +9,7 @@ module.exports = {
     '\\.(css|scss)$': '<rootDir>/src/__tests__/App.js',
   },
   setupFilesAfterEnv: ['./src/setupTestSuite.js'],
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  coverageDirectory: '<rootDir>/src/__tests__/coverage',
   coverageReporters: ['json', 'lcov', 'text', 'clover', 'cobertura'],
 };
