a34b1cfd8bd49c364532e0743f4b3edb757e019c
Mac <mac@MacBook-Pro.local>
2021-09-01 18:51:35 -0700

    init component

---
+++ src/components/ConservationPracticesCategories/ConservationPracticesCategories.tsx
@@ -0,0 +1,12 @@
+const ConservationPracticesCategories = () => {
+    return (
+        <div>
+            <div>Conservation Practice Categories</div>
+            <div>Conservation Practice Categories introduction ...</div>
+            <div>categories list</div>
+    </div>
+)
+
+}
+
+export default ConservationPracticesCategories;
