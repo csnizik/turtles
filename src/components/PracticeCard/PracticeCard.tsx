a0917838ab0ae01327279d6901df9051a8891b69
Ahmad <ahmzad14465@hotmail.com>
2021-09-08 15:16:20 -0400

    API Call

--- src/components/PracticeCard/PracticeCard.tsx
+++ src/components/PracticeCard/PracticeCard.tsx
@@ -1,34 +1,35 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './practice-card.scss';
-
-interface IPracticeData {
-  practiceId: number;
-  practiceName: string;
-  practiceDescription: string;
-  practiceLink: string;
-}
-
-interface ICategoryData {
-  practiceCategoryId: string;
-}
+// import { usePostSearchDataQuery } from '../../Redux/services/api';
+// import { ISearchData } from '../../common/types';
 
 const PracticeCardDetails = () => {
+  // const location: any = useLocation ();
+
+  // const sharedState = practiceCategoryId
+
+  // const {data, error, isLoading, isSucess, isError} =
+  // usePostSearchDataQuery(sharedState);
+
   return (
     <>
       <h2>36 Practices</h2>
+      {/* {data.map((practice: ISearchData) => {})} */}
       <div className='full-document-box'>
         <div className='list-box'>
           <div className='info-box'>
-            <Link to='#'>
+            <Link
+              to={{
+                pathname: '#',
+                // location: practiceId,
+              }}
+            >
               <h4>Alley Cropping</h4>
             </Link>
             <p>
               Alley cropping is an agroforestry practice where agricultural or
               horticultural crops are grown in the alleyways between widely
-              spaced rows of woody plants. By combining annual and perennial
-              crops that yield varied products and profits at different times, a
-              landowner can more effectively use available space, time, and
-              resources.
+              spaced rows of woody plants.
             </p>
           </div>
           <img
