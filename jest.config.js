63f59b2f96afe9eb4157e2d2151037944032a2a8
Mac <mac@MacBook-Pro.local>
2021-09-29 11:37:20 -0700

    fix tests for ReportBuilder, fix toBeInTheDocument

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,7 @@ module.exports = {
     '\\.(css|scss)$': '<rootDir>/src/__tests__/App.js',
     '\\.svg': '<rootDir>/src/__mocks__/svgrMock.js',
   },
-  setupFilesAfterEnv: ['./src/setupTestSuite.js'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTestSuite.js'],
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
   //coverageDirectory: '<rootDir>/src/__tests__/coverage',
   coverageReporters: ['json', 'lcov', 'text', 'clover', 'cobertura'],
