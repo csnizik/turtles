2a841b0a5f6f8dfc3c40d93f5b3c4916b6a8dcb9
Fei Wang <fei.wang@spatialfront.com>
2021-09-29 23:07:23 -0400

    update tableauimage file

--- src/components/ResourceConcernTreated/DummyTableauImage.tsx
+++ src/components/ResourceConcernTreated/DummyTableauImage.tsx
@@ -1,15 +1,40 @@
+import { useState } from 'react';
 
 const DummyTableauImage = () => {
+  // state was manually chosen as "Colorado" and the top five
+  const tableauUrl =
+    'https://publicdashboards.dl.usda.gov/t/FPAC_PUB/views/EQIPTopPracticesTest/TopPractices.png?State%20Name%20with%20Total=Colorado&Top%20Practice%20Rank=1,2,3,4,5';
+  const [base64Url, setBase64Url] = useState('');
+  async function parseURI(d) {
+    const reader = new FileReader();
+    reader.readAsDataURL(d);
+    // eslint-disable-next-line no-unused-vars
+    return new Promise((res, rej) => {
+      reader.onload = (e: any) => {
+        res(e.target.result);
+      };
+    });
+  }
+
+  async function getDataBlob(url) {
+    const res = await fetch(url);
+    const blob = await res.blob();
+    const uri: any = await parseURI(blob);
+    setBase64Url(uri);
+  }
+
+  getDataBlob(tableauUrl);
 
   return (
     <>
-      <img 
+      {/* <img 
         alt='Tableau Icon'
         // eslint-disable-next-line global-require
-        src={require('./image/Tableau-IconPlaceholder.png').default}
-      />
+        src={require('./image/Tableau-IconPlaceholder.png').default}      
+      /> */}
+      <img alt='Tableau Icon' src={base64Url} />
     </>
   );
-}
+};
 
 export default DummyTableauImage;
