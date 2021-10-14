3b3241552074bb26d910799ae1421f01c5b600b5
Ahmad <ahmzad14465@hotmail.com>
2021-10-14 17:47:06 -0400

    Pagination Component added

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,14 +1,22 @@
 import './pagination.scss';
 
 const Pagination = () => {
+  const handlePrevClick = () => {
+    return 0;
+  };
+
+  const handleNextClick = () => {
+    return 0;
+  };
+
   return (
     <>
-      <h3 className='site-preview-heading'>Default</h3>
+      <h3 className='site-preview-heading'>Showing 1 - 10 of 757 projects</h3>
       <nav aria-label='Pagination' className='usa-pagination'>
         <ul className='usa-pagination__list'>
           <li className='usa-pagination__item usa-pagination__arrow'>
             <a
-              //   href='javascript:void(0);'
+              href='#'
               className='usa-pagination__link usa-pagination__previous-page'
               aria-label='Previous page'
             >
@@ -16,15 +24,21 @@ const Pagination = () => {
                 <use href='/assets/img/sprite.svg#navigate_before' />
               </svg>
 
-              <span className='usa-pagination__link-text'> Previous </span>
+              <span
+                className='usa-pagination__link-text'
+                onClick={() => handlePrevClick()}
+              >
+                Previous
+              </span>
             </a>
           </li>
 
           <li className='usa-pagination__item usa-pagination__page-no'>
             <a
-              //   href='javascript:void(0);'
-              className='usa-pagination__button'
+              href='#'
+              className='usa-pagination__button usa-current'
               aria-label='Page 1'
+              aria-current='page'
             >
               1
             </a>
@@ -38,60 +52,35 @@ const Pagination = () => {
           </li>
 
           <li className='usa-pagination__item usa-pagination__page-no'>
-            <a
-              //   href='javascript:void(0);'
-              className='usa-pagination__button'
-              aria-label='Page 9'
-            >
+            <a href='#' className='usa-pagination__button' aria-label='Page 9'>
               9
             </a>
           </li>
 
           <li className='usa-pagination__item usa-pagination__page-no'>
-            <a
-              //   href='javascript:void(0);'
-              className='usa-pagination__button usa-current'
-              aria-label='Page 10'
-              aria-current='page'
-            >
+            <a href='#' className='usa-pagination__button' aria-label='Page 10'>
               10
             </a>
           </li>
 
           <li className='usa-pagination__item usa-pagination__page-no'>
-            <a
-              //   href='javascript:void(0);'
-              className='usa-pagination__button'
-              aria-label='Page 11'
-            >
+            <a href='#' className='usa-pagination__button' aria-label='Page 11'>
               11
-            </a>
-          </li>
-
-          <li
-            className='usa-pagination__item usa-pagination__overflow'
-            role='presentation'
-          >
-            <span> … </span>
-          </li>
-
-          <li className='usa-pagination__item usa-pagination__page-no'>
-            <a
-              //   href='javascript:void(0);'
-              className='usa-pagination__button'
-              aria-label='Last page, page 24'
-            >
-              24
             </a>
           </li>
 
           <li className='usa-pagination__item usa-pagination__arrow'>
             <a
-              //   href='javascript:void(0);'
+              href='#'
               className='usa-pagination__link usa-pagination__next-page'
               aria-label='Next page'
             >
-              <span className='usa-pagination__link-text'> Next </span>
+              <span
+                className='usa-pagination__link-text'
+                onClick={() => handleNextClick()}
+              >
+                Next
+              </span>
 
               <svg className='usa-icon' aria-hidden='true' role='img'>
                 <use href='/assets/img/sprite.svg#navigate_next' />
