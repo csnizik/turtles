d0c24fc1c1299f9c3e26b086e19ed1da4927e46c
Ahmad <ahmzad14465@hotmail.com>
2021-10-19 12:08:28 -0400

    Associated Practice Done

--- src/components/SpecificationsAndTools/SpecificationsAndTools.tsx
+++ src/components/SpecificationsAndTools/SpecificationsAndTools.tsx
@@ -1,8 +1,13 @@
+import { useTranslation } from 'react-i18next';
+import { useGetAssociatedPracticeQuery } from '../../Redux/services/api';
 import './specs.scss';
+import { IAssociatedPracticeList } from '../../common/types';
 
 interface ISpecAndToolsProps {
   data: any;
   isSuccess: boolean;
+  selectedStateCode: string;
+  selectedPracticeId: number;
 }
 
 const intro: string =
@@ -10,7 +15,25 @@ const intro: string =
 const promptText: string =
   'You can find national conservation practice standards, overviews, conservation practice effects and network effects diagrams on the NRCS website.';
 
-const SpecificationsAndTools = ({ data, isSuccess }: ISpecAndToolsProps) => {
+const SpecificationsAndTools = ({
+  data,
+  isSuccess,
+  selectedStateCode,
+  selectedPracticeId,
+}: ISpecAndToolsProps) => {
+  const userSelectedFilter = {
+    stateCode: selectedStateCode,
+    practiceId: selectedPracticeId,
+  };
+
+  const { t } = useTranslation();
+
+  const clickHandler = (value) => {
+    window.localStorage.setItem('Practice', value);
+  };
+
+  const content = useGetAssociatedPracticeQuery(userSelectedFilter);
+
   const getHeaderText = () => {
     const practiceName = (data && data?.practiceName) || '';
     if (practiceName) {
@@ -21,25 +44,72 @@ const SpecificationsAndTools = ({ data, isSuccess }: ISpecAndToolsProps) => {
 
   const renderNationalSpecs = () => {
     return (
-      <div className='national-specs'>
-        <h4>National Specifications</h4>
-        <h5>{promptText}</h5>
-        <div className='link'>
-          <a
-            href='https://www.nrcs.usda.gov/wps/portal/nrcs/detailfull/national/technical/cp/ncps/?cid=nrcs143_026849'
-            target='_blank'
-            rel='noopener noreferrer'
-            aria-label='Current NRCS National Conservation Practices link'
-          >
-            NRCS National Conservation Practices
-            <img
-              alt='All Conservation at Work videos'
-              // eslint-disable-next-line global-require
-              src={require('./image/newLinkIcon.svg').default}
-            />
-          </a>
+      <>
+        <div className='national-specs'>
+          <h4>National Specifications</h4>
+          <h5>{promptText}</h5>
+          <div className='link'>
+            <a
+              href='https://www.nrcs.usda.gov/wps/portal/nrcs/detailfull/national/technical/cp/ncps/?cid=nrcs143_026849'
+              target='_blank'
+              rel='noopener noreferrer'
+              aria-label='Current NRCS National Conservation Practices link'
+            >
+              NRCS National Conservation Practices
+              <img
+                alt='All Conservation at Work videos'
+                // eslint-disable-next-line global-require
+                src={require('./image/newLinkIcon.svg').default}
+              />
+            </a>
+          </div>
         </div>
-      </div>
+
+        <div className='associated-prac'>
+          <h4>{t('associated-practices.title')}</h4>
+          <p>{t('associated-practices.description')}</p>
+          <div className='row'>
+            {content.data?.map((practice: any) => {
+              return (
+                <>
+                  <div className='grid-col-6'>
+                    <ul>
+                      <li key={practice.practiceId}>
+                        <a
+                          href='/ConservationPractices'
+                          onClick={() => clickHandler(practice.practiceId)}
+                          target='_blank'
+                        >
+                          {practice.practiceName}
+                        </a>
+                        &ensp;({practice.practiceCode})
+                      </li>
+                    </ul>
+                  </div>
+                  {practice.data?.map((item: IAssociatedPracticeList) => {
+                    return (
+                      <div className='grid-col-6'>
+                        <ul>
+                          <li key={item.practiceId}>
+                            <a
+                              href='/ConservationPractices'
+                              onClick={() => clickHandler(item.practiceId)}
+                              target='_blank'
+                            >
+                              {item.practiceName}
+                            </a>
+                            &ensp;({item.practiceCode})
+                          </li>
+                        </ul>
+                      </div>
+                    );
+                  })}
+                </>
+              );
+            })}
+          </div>
+        </div>
+      </>
     );
   };
 
