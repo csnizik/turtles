00c191fee2bf10fa643e9e4b3c68c4bdfda94d5c
Brandon Beschta <brandonbeschta@Brandons-MacBook-Pro.local>
2021-10-20 16:27:44 -0500

    Add composite view

--- src/containers/MapContainer/MapComponent.tsx
+++ src/containers/MapContainer/MapComponent.tsx
@@ -1,16 +1,15 @@
 import { useEffect, useRef } from 'react';
 import MapView from '@arcgis/core/views/MapView';
 import Home from '@arcgis/core/widgets/Home';
+import Graphic from '@arcgis/core/Graphic';
 import Map from '@arcgis/core/Map';
-import MapImageLayer from '@arcgis/core/layers/MapImageLayer';
+import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
 import {
-  MAP_ZOOM,
   MIN_ZOOM,
   MAX_ZOOM,
-  MAX_LEFT_POSITION,
-  MAX_RIGHT_POSITION,
-  CENTER_COORDINATES,
   VIEW_DIV,
+  simpleFillColor,
+  fillBorderColor,
 } from './constants';
 import '@arcgis/core/assets/esri/themes/light/main.css';
 
@@ -18,11 +17,17 @@ interface IMapProps {
   view: MapView;
 }
 
-const albersStateURL: any =
+const statesFeatureLayerURL: any =
   'https://gis1-eft.spatialfrontlab.com/arcgis/rest/services/cig-cpd/ALBERS_States/MapServer';
 
 const MapComponent = () => {
   const mapRef = useRef({} as IMapProps);
+  const statesLayer = useRef(
+    new FeatureLayer({
+      url: statesFeatureLayerURL,
+      layerId: 1,
+    })
+  );
   useEffect(() => {
     if (mapRef && mapRef.current) {
       const map = new Map();
@@ -36,34 +41,84 @@ const MapComponent = () => {
         view,
       });
 
-      // Set view constraints
-      view.constraints = {
-        minZoom: MIN_ZOOM,
-        maxZoom: MAX_ZOOM,
-      };
-
       mapRef.current.view = view;
       // Add the home button to the top left corner of the view
       mapRef.current.view.ui.add(homeBtn, 'top-left');
-      const layer = new MapImageLayer(albersStateURL);
-      map.layers.add(layer);
+
+      const akView = new MapView({
+        container: 'akViewDiv',
+        map: map,
+        extent: {
+          // autocasts as new Extent()
+          xmin: -5439870.428998005,
+          ymin: 3933143.727441013,
+          xmax: -2211170.354233005,
+          ymax: 6203017.719397005,
+          spatialReference: {
+            wkid: 102965,
+          },
+        },
+        spatialReference: {
+          // WGS 1984 Alaska Polar Stereographic
+          // projected coordinate system
+          wkid: 102965,
+        },
+        ui: {
+          // clears all default widgets from the
+          // inset view
+          components: [],
+        },
+      });
+      // Add the alaska view container as an inset view
+      mapRef.current.view.ui.add('akViewDiv', 'bottom-left');
+
+      // Add Feature Layer
+      map.layers.add(statesLayer.current);
     }
+  }, [mapRef]);
 
-    mapRef.current.view.on('pointer-down', (event) => {
-      console.log('Pointer down Event: ', event);
-    });
+  useEffect(() => {
+    mapRef.current.view.when(() => {
+      mapRef.current.view.on('pointer-up', (event) => {
+        mapRef.current.view.hitTest(event).then((response) => {
+          mapRef.current.view.graphics.removeAll();
+          if (response.results.length) {
+            console.log('response: ', response);
+            const graphicList: any = response.results.filter((item: any) => {
+              // check if the graphic belongs to the states layer
+              if (statesLayer.current) {
+                return item.graphic.layer === statesLayer.current;
+              }
+              return response.results;
+            });
 
-    mapRef.current.view.on('drag', (event) => {
-      if (event) {
-        // Check if the user is trying to navigate outside the USA
-        console.log('Event: ', event);
-        // if (
-        //   event.origin.x > MAX_RIGHT_POSITION &&
-        //   event.origin.x < MAX_LEFT_POSITION
-        // ) {
-        //   event.stopPropagation();
-        // }
-      }
+            if (graphicList.length) {
+              const selectedState: Graphic = graphicList[0].graphic;
+              // Highlight selected state
+              mapRef.current.view.graphics.removeAll();
+
+              const highlightSymbol = {
+                type: 'simple-fill',
+                color: simpleFillColor,
+                style: 'solid',
+                outline: {
+                  color: fillBorderColor,
+                  width: 1,
+                },
+              };
+
+              const selectedGraphic = new Graphic({
+                geometry: selectedState.geometry,
+                symbol: highlightSymbol,
+              });
+              mapRef.current.view.graphics.add(selectedGraphic);
+
+              // Zoom to selected state
+              mapRef.current.view.goTo({ target: selectedState, zoom: 5 });
+            }
+          }
+        });
+      });
     });
   }, [mapRef]);
 
