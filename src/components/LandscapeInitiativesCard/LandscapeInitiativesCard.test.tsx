43010fdd9e65f117027ae14535941819d38f330b
Ahmad <ahmzad14465@hotmail.com>
2021-12-07 14:22:12 -0500

    Adding clickable titles to LCI cards

--- src/components/LandscapeInitiativesCard/LandscapeInitiativesCard.test.tsx
+++ src/components/LandscapeInitiativesCard/LandscapeInitiativesCard.test.tsx
@@ -1,5 +1,10 @@
 import LandscapeInitiativesCard from './LandscapeInitiativesCard';
-import { cleanup, render, screen } from '../../common/test-utils/test_utils';
+import {
+  cleanup,
+  fireEvent,
+  render,
+  screen,
+} from '../../common/test-utils/test_utils';
 
 afterEach(() => {
   cleanup();
@@ -8,25 +13,39 @@ afterEach(() => {
 describe('Landscape Initiatives Card is rendered correctly', () => {
   const lci_name = 'Golden-Winged Warbler';
   const lci_description = ['Identified target species'];
+  const lci_link = 'https://www.nrcs.usda.gov/';
+  const sub_name = 'Gopher Tortoise';
 
   beforeEach(() => {
     render(
       <LandscapeInitiativesCard
         title={lci_name}
         description={lci_description}
+        link={lci_link}
       />
     );
   });
 
-  test('Should display the Card Component', () => {
+  test('Should display the Landscape Initiatives Card Component', () => {
     expect(screen.getByTestId('subInitiativesCard')).toBeDefined();
   });
-  test('Should display the Card Title', () => {
+  test('Should display the Landscape Initiatives Card Title', () => {
     expect(screen.getByTestId('subInitiativesTitle')).toBeDefined();
     expect(screen.getByText('Golden-Winged Warbler')).toBeInTheDocument();
   });
-  test('Should display the Card Description', () => {
+  test('Should display the Landscape Initiatives Card Description', () => {
     expect(screen.getByTestId('subInitiativesDescription')).toBeDefined();
     expect(screen.getByText('Identified target species')).toBeInTheDocument();
+  });
+  test('Should test Landscape Initiatives Card Link', () => {
+    const { getByText } = render(
+      <a href={`https://www.nrcs.usda.gov/`}>{sub_name}</a>
+    );
+    const link = getByText('Gopher Tortoise');
+    fireEvent.click(link);
+    fireEvent.mouseOver(link);
+    expect(
+      screen.getByText('Golden-Winged Warbler').closest('a')
+    ).toHaveAttribute('href', 'https://www.nrcs.usda.gov/');
   });
 });
