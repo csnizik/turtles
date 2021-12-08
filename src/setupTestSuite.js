0f60617b9d8c41376cc42341ad5e3c642d74e3a9
Zane Belkhayat <zane.belkhayat@spatialfront.com>
2021-12-08 15:25:09 -0500

    working on msw

--- src/setupTestSuite.js
+++ src/setupTestSuite.js
@@ -1,5 +1,10 @@
 import '@testing-library/jest-dom/extend-expect';
+import 'regenerator-runtime/runtime';
+import { mswServer } from './api-mocks/msw-server';
 
+beforeAll(() => mswServer.listen());
+afterEach(() => mswServer.resetHandlers());
+afterAll(() => mswServer.close());
 jest.mock('../jest.config.js');
 
 // Mock external components and functions
