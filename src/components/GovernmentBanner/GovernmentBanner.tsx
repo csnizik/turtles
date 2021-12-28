54179e95177c32b28e5fd125d82b2aa383a5d272
Eddie Ironsmith <eddie.ironsmith@spatialfront.com>
2021-12-28 16:45:43 -0600

    Replace banner for CIG-1318 typo

--- src/components/GovernmentBanner/GovernmentBanner.tsx
+++ src/components/GovernmentBanner/GovernmentBanner.tsx
@@ -77,7 +77,7 @@ const GovernmentBanner = () => {
               <div className="usa-media-block__body">
                 <p>
                   <strong>The .gov means it is official.</strong>
-                  <br></br>
+                  <br/>
                   Federal government websites often end in .gov or .mil. Before sharing sensitive information, make sure you’re on a federal government site.
                 </p>
               </div>
@@ -91,7 +91,7 @@ const GovernmentBanner = () => {
               <div className="usa-media-block__body">
                 <p>
                   <strong>The site is secure.</strong>
-                  <br></br>
+                  <br/>
                   The <strong>https://</strong> ensures that you are connecting to the official website and that any information you provide is encrypted and transmitted securely.
                 </p>
               </div>
@@ -99,7 +99,6 @@ const GovernmentBanner = () => {
           </div>
         </div>
       </div>
-
       <div className='usda-header row-flex-start'>
         <img src='../../../../images/usda_logo_color.png' alt='USDA LOGO' />
         <div className='government-banner'>
@@ -109,7 +108,6 @@ const GovernmentBanner = () => {
           <p>{t('header.usda')}</p>
         </div>
       </div>
-      
     </header>
   );
 
