1a78fade86922cf19ee518f7c87866e098daa3c9
Ahmad <ahmzad14465@hotmail.com>
2022-01-21 15:47:53 -0500

    Location Unit test added

--- src/components/SearchByLocation/SearchByLocation.test.tsx
+++ src/components/SearchByLocation/SearchByLocation.test.tsx
@@ -1,20 +1,46 @@
 import SearchByLocation from './SearchByLocation';
-import { cleanup, render, screen } from '../../common/test-utils/test_utils';
+import {
+  cleanup,
+  fireEvent,
+  render,
+  screen,
+} from '../../common/test-utils/test_utils';
+import { createTestStore } from '../../Redux/store';
+import { Provider } from 'react-redux';
 
 afterEach(() => {
   cleanup();
 });
 
+let store;
 let setSearchInput = jest.fn;
 let setSearchInfo = jest.fn;
 
 describe('SearchByLocation is rendered correctly', () => {
-  beforeEach(() => {
-    render(<SearchByLocation setSearchInput = {setSearchInput}
-        setSearchInfo= {setSearchInfo}/>);
-  });
+  store = createTestStore();
 
-  test('Should display the contents of SearchByLocation', () => {
+  test('Should test dropdown and state selection functionality', async () => {
+    const { findByText } = render(
+      <Provider store={store}>
+        <SearchByLocation
+          setSearchInput={setSearchInput}
+          setSearchInfo={setSearchInfo}
+        />
+      </Provider>
+    );
     expect(screen.getByTestId('location-search')).toBeDefined();
+
+    await findByText('Colorado');
+
+    fireEvent.click(screen.getByText('Colorado'));
+
+    fireEvent.change(screen.getByTestId('select'), {
+      target: { value: '08' },
+    });
+
+    expect(screen.getByTestId('select')).toHaveValue('08');
+
+    await findByText('actions.clear');
+    fireEvent.click(screen.getByText('actions.clear'));
   });
 });
