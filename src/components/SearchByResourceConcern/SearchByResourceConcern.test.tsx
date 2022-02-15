14715d3039116b83b037cddbee2a21b337f2c9e2
Mac <mac@MacBook-Pro.local>
2022-02-15 11:57:34 -0800

    remove console.log

--- src/components/SearchByResourceConcern/SearchByResourceConcern.test.tsx
+++ src/components/SearchByResourceConcern/SearchByResourceConcern.test.tsx
@@ -24,10 +24,71 @@ let selectedResourceConcern = {};
 let setSelectedResourceConcern = spy;
 let store;
 
-describe('SearchByResourceConcern is rendered correctly', () => {
+describe('SearchByResourceConcern, resource concern selected', () => {
   store = createTestStore();
 
-  test('Should test dropdown and resource category selection functionality', async () => {
+  test('Resource concern treated should pass selected resource concern value to parent', async () => {
+    const { findByText } = render(
+      <Provider store={store}>
+        <SearchByResourceConcern
+          setSearchInput={setSearchInput}
+          setSearchInfo={setSearchInfo}
+          setResourceConcernsSubgroups={setResourceConcernsSubgroups}
+          selectedResourceCategory={selectedResourceCategory}
+          setSelectedResourceCategory={setSelectedResourceCategory}
+          selectedPractice={selectedPractice}
+          practiceId={practiceId}
+          selectedResourceConcern={selectedResourceConcern}
+          setSelectedResourceConcern={setSelectedResourceConcern}
+        />
+      </Provider>
+    );
+
+    await findByText('Emissions of ozone precursors');
+
+    fireEvent.click(screen.getByText('Emissions of ozone precursors'));
+
+    fireEvent.change(screen.getByTestId('subselect'), {
+      target: { value: 180 },
+    });
+    expect(setSelectedResourceConcern).toHaveBeenCalledWith({ id: 180 });
+  });
+});
+
+describe('SearchByResourceConcern, resource concern default', () => {
+  store = createTestStore();
+
+  test('Resource concern treated should pass default selected resource concern value to parent', async () => {
+    const { findByText } = render(
+      <Provider store={store}>
+        <SearchByResourceConcern
+          setSearchInput={setSearchInput}
+          setSearchInfo={setSearchInfo}
+          setResourceConcernsSubgroups={setResourceConcernsSubgroups}
+          selectedResourceCategory={selectedResourceCategory}
+          setSelectedResourceCategory={setSelectedResourceCategory}
+          selectedPractice={selectedPractice}
+          practiceId={practiceId}
+          selectedResourceConcern={selectedResourceConcern}
+          setSelectedResourceConcern={setSelectedResourceConcern}
+        />
+      </Provider>
+    );
+
+    await findByText('- Select resource concern -');
+
+    fireEvent.click(screen.getByText('- Select resource concern -'));
+
+    fireEvent.change(screen.getByTestId('subselect'), {
+      target: { value: '' },
+    });
+    expect(setSelectedResourceConcern).toHaveBeenCalledWith({ id: -1 });
+  });
+});
+
+describe('SearchByResourceConcern, resource category selected', () => {
+  store = createTestStore();
+  test('Resource Concerns Treated should pass selected resource category value to parent', async () => {
     const { findByText } = render(
       <Provider store={store}>
         <SearchByResourceConcern
@@ -45,25 +106,44 @@ describe('SearchByResourceConcern is rendered correctly', () => {
     );
 
     await findByText('Air');
-
     fireEvent.click(screen.getByText('Air'));
-
     fireEvent.change(screen.getByTestId('select'), {
       target: { value: '3' },
     });
-
-    expect(setSelectedResourceCategory).toHaveBeenCalled();
-
-    await findByText('Emissions of ozone precursors');
-
-    fireEvent.click(screen.getByText('Emissions of ozone precursors'));
-
-    fireEvent.change(screen.getByTestId('subselect'), {
-      target: { value: '180' },
-    });
-    expect(setSelectedResourceConcern).toHaveBeenCalled();
+    expect(setSelectedResourceCategory).toHaveBeenCalledWith({ id: '3' });
   });
+});
 
+describe('SearchByResourceConcern, resource category default', () => {
+  store = createTestStore();
+  test('Resource Concerns Treated should pass selected default resource category value to parent', async () => {
+    const { findByText } = render(
+      <Provider store={store}>
+        <SearchByResourceConcern
+          setSearchInput={setSearchInput}
+          setSearchInfo={setSearchInfo}
+          setResourceConcernsSubgroups={setResourceConcernsSubgroups}
+          selectedResourceCategory={selectedResourceCategory}
+          setSelectedResourceCategory={setSelectedResourceCategory}
+          selectedPractice={selectedPractice}
+          practiceId={practiceId}
+          selectedResourceConcern={selectedResourceConcern}
+          setSelectedResourceConcern={setSelectedResourceConcern}
+        />
+      </Provider>
+    );
+
+    await findByText('All resource concerns (default)');
+    fireEvent.click(screen.getByText('All resource concerns (default)'));
+    fireEvent.change(screen.getByTestId('select'), {
+      target: { value: '' },
+    });
+    expect(setSelectedResourceCategory).toHaveBeenCalledWith({ id: -1 });
+  });
+});
+
+describe('SearchByResourceConcern is rendered correctly', () => {
+  store = createTestStore();
   test('Should display the contents of SearchByResourceConcern', () => {
     render(
       <Provider store={store}>
